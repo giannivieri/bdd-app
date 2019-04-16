0ff94fb10824b2fd41d6cfb4ba932db5fd288261
gvieri <gianni@adabra.com>
2019-04-16 09:31:10 +0200

    Describe book inventory listing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'support/controller_helpers'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -74,6 +75,9 @@ RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
+    config.include Warden::Test::Helpers
+    config.include Devise::TestHelpers, :type => :controller
+    config.include ControllerHelpers, :type => :controller
 end
   config.around(:each) do |example|
     DatabaseCleaner.cleaning do
